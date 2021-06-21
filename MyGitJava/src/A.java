445b6d4c8a83d5dda2b04f433c2ff54fde73d4e1
ASROCK <ASROCK@59.5.55.58>
2021-06-21 23:36:43 +0900

    First commit

---
+++ MyGitJava/src/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git!");
+		
+		System.out.println("This is A.java");
+
+	}
+
+}
---
+++ MyGitJava/src/B.java
@@ -0,0 +1,11 @@
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git!");
+		
+		System.out.println("This is B.java");
+
+	}
+
+}
